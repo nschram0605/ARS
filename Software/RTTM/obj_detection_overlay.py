af4ff5a52504adafb3303c4694a26d01347a998d
neo <nschram05@gmail.com>
2018-04-22 16:06:31 -0600

    Functioning circle detection on grid.

--- Software/RTTM/obj_detection_overlay.py
+++ Software/RTTM/obj_detection_overlay.py
@@ -25,6 +25,8 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+#from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
 import scipy
 import seaborn
 
@@ -66,7 +68,9 @@ f_circle_pixels = [832, 192]
 
 # ----------------------------------------------------------------------------------------------------------------------
 def get_centerpoint_location_for_plotting(location):
+
     print ('Location: ', location)
+
     if (location == 'A'):
         x_val = a_circle[0]
         y_val = a_circle[1]
@@ -84,7 +88,7 @@ def get_centerpoint_location_for_plotting(location):
         y_val = d_circle[1]
         print('---> x_val: ', x_val, " y_val: ", y_val)
     elif (location == 'E'):
-        x_val = e_circle[0]
+        x_val = e_cirlce[0]
         y_val = e_circle[1]
         print('---> x_val: ', x_val, " y_val: ", y_val)
     elif (location == 'F'):
@@ -92,16 +96,113 @@ def get_centerpoint_location_for_plotting(location):
         y_val = f_circle[1]
         print('---> x_val: ', x_val, " y_val: ", y_val)
 
-    plot_detection_circle(x_val, y_val, circle_r)
+    #plot_detection_circle(x_val, y_val, circle_r)
+    create_test_grid(x_val, y_val, circle_r)
 
     # ------------------------------------------------->
+
+def create_grid():
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    major_ticks_x = np.arange(0, global_x + 1, 5)
+    minor_ticks_x = np.arange(0, global_x + 1, 1)
+
+    major_ticks_y = np.arange(0, global_y + 1, 5)
+    minor_ticks_y = np.arange(0, global_y + 1, 1)
+
+    # v = [-global_x, global_x, -global_y, global_y]
+    # ax.axis(v)
+
+    # ax.set_xlim(-global_x, global_x)
+    # ax.set_ylim(-global_y, global_y)
+
+    ax.set_xticks(major_ticks_x)
+    ax.set_xticks(minor_ticks_x, minor=True)
+
+    ax.set_yticks(major_ticks_y)
+    ax.set_yticks(minor_ticks_y, minor=True)
+
+    # ax.axis('scaled')
+
+    ax.grid(which='both')
+
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+
+    plt.show()
+
+
+def create_test_grid(x, y, circle_radius):
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.axis('scaled')
+
+    major_ticks_x = np.arange(0, global_x + 1, 5)
+    minor_ticks_x = np.arange(0, global_x + 1, 1)
+
+    major_ticks_y = np.arange(0, global_y + 1, 5)
+    minor_ticks_y = np.arange(0, global_y + 1, 1)
+
+    # v = [-global_x, global_x, -global_y, global_y]
+    # ax.axis(v)/home/nathan/anaconda3/bin/python3.6 /home/nathan/PycharmProjects/ARS/Software/RTTM/obj_detection_overlay.py
+
+
+    # ax.set_xlim(-global_x, global_x)
+    # ax.set_ylim(-global_y, global_y)
+
+    ax.set_xticks(major_ticks_x)
+    ax.set_xticks(minor_ticks_x, minor=True)
+
+    ax.set_yticks(major_ticks_y)
+    ax.set_yticks(minor_ticks_y, minor=True)
+
+    #ax.axis('scaled')
+    # ax.plt.figure(figsize=(global_x, global_y))
+
+    ax.grid(which='both')
+
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+
+    circle = plt.Circle((x, y), linewidth=5, radius=circle_radius, color='red', fill=False)
+    plt.gcf().gca().add_artist(circle)
+
+    plt.show()
+
 def plot_detection_circle(x, y, circle_radius):
 
-    print ("X: ", x, " Y: ", y)
+    '''
+    plt.axes()
+    circle = plt.Circle((x, y), linewidth=5, radius=circle_radius, color='red', fill=False)
+    # .figure(figsize=(global_x, global_y))
+    # plt.figure(figsize=(global_x, global_y))
+    v = [-global_x, global_x, -global_y, global_y]
 
-    circle1 = plt.Circle((x, y), circle_radius, color='red')
+    plt.gca().add_patch(circle)
+    plt.axis('scaled')
+    plt.axis(v)
+    plt.grid(True)
+    plt.title('Object Detection')
+    plt.xlabel('X Axis')
+    plt.ylabel('Y Axis')
+    plt.show()
+    '''
 
-    fig, ax = plt.subplots()  # note we must use plt.subplots, not plt.subplot
+'''
+    print ("X: ", x, " Y: ", y, " Circle Radius (in.): ", circle_radius)
+
+    circle1 = plt.Circle((x, y), circle_radius, color='red', fill=False)
+
+    #fig, ax = plt.subplots()  # note we must use plt.subplots, not plt.subplot
+    #plt.figure(figsize=(global_x, global_y))
+    print("Sandbox Dimensions:", global_x, "x", global_y)
+    plt.figure(num=1, figsize=(global_x, global_y), facecolor='w')
+    plt.gcf().gca().add_artist(circle1)
 
     # for existing figure
     # plt.gcf().gca().add_artist(circle1)
@@ -109,8 +210,8 @@ def plot_detection_circle(x, y, circle_radius):
     # fig = plt.gcf()
     # ax = fig.gca()
     plt.grid(True)
-    plt.figure(figsize=(x, y))
-    ax.add_artist(circle1)
+    # plt.figure(figsize=(x, y))
+    #ax.add_artist(circle1)
 
     plt.show()
     #fig.savefig('plotcircles.png')
@@ -140,9 +241,44 @@ def plot_detection_circle(x, y, circle_radius):
     # plt.savefig("sin_wave.png", transparent=True)
     #plt.show()
     return
-
+'''
 # <-------------------------------------------------
+
+'''
+def create_circle():
+    circle=plt.Circle(0,0), radius=5)
+    return circle
+
+def show_shape(patch):
+    ax=plt.gca()
+    ax.add_patch(patch)
+    plt.axis('scaled')
+    plt.show()
+'''
+# ----------------------------------------------------------------------
 
 get_centerpoint_location_for_plotting(location)
 
+#create_grid()
 #plot_detection_circle(4, 3, 10)
+
+#c=create_circle()
+#show_shape(c)
+
+#create_test_grid()
+
+'''
+plt.axes()
+circle = plt.Circle((0, 0), linewidth=5, radius=0.75, color='red', fill=False)
+#.figure(figsize=(global_x, global_y))
+#plt.figure(figsize=(global_x, global_y))
+v = [-global_x, global_x, -global_y, global_y]
+
+plt.gca().add_patch(circle)
+plt.axis('scaled')
+plt.axis(v)
+plt.grid(True)
+plt.title('Object Detection')
+plt.xlabel('X Axis')
+plt.ylabel('Y Ax
+'''
