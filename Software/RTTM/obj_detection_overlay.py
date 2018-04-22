84cf0ec1ed65c9f5033ed8b09a5e1258e55f83d2
nschram05 <nschram05@gmail.com>
2018-04-22 11:06:15 -0600

    Working on obj_detection_overlay.py

--- Software/RTTM/obj_detection_overlay.py
+++ Software/RTTM/obj_detection_overlay.py
@@ -1,14 +1,12 @@
-#
-# obj_detection_overlay.py
-#
-
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy
-import seaborn
-import plotly.plotly as py
-import plotly.tools as tls
+###########################################################################################
+###
+### obj_detection_overlay.py
+###
+### nathan schram
+###
+### the purpose of this program is to plot a circle in red of diameter 15".
+### the function will take the x, y location information for the center of the circle.
+###########################################################################################
 
 # 40"x30" inner dimensions
 #
@@ -20,37 +18,113 @@ import plotly.tools as tls
 # -------------------      |
 # |     |     |     | 15"  |
 # |D    |E    |F    |      |
-#--------------------     ---
-#
+# -------------------     ---
+
+# Projector outputs the image size in pixels: 1024x768
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import seaborn
+
+# Position of circle, this comes from the detector circuit
+# Object detection location: will change the location variable (A-F)
+location = 'A'
+
+# Detector circle dimensions in inches
+circle_r = 7.5
+circle_d = 15
+circle_r_pixels = 192
+circle_d_pixels = 384
+
+# global sandbox size in inches & pixels
+global_x = 40
+global_y = 30
+global_x_pixels = 1024
+global_y_pixels = 768
+
+# Define all position variables for circles in inches
+a_circle = [7.5, 22.5]
+b_circle = [20, 22.5]
+c_circle = [32.5, 22.5]
+d_circle = [7.5, 7.5]
+e_circle = [20, 7.5]
+f_circle = [32.5, 7.5]
+
+# circle_r_pixels = 192
+# pixel equivalent of 20: 512
+# pixel equivalent of 22.5: 576
+# pixel equivalent of 32.5: 832
+
+a_circle_pixels = [192, 576]
+b_circle_pixels = [512, 576]
+c_circle_pixels = [832, 576]
+d_circle_pixels = [192, 192]
+e_circle_pixels = [512, 192]
+f_circle_pixels = [832, 192]
+
+# ----------------------------------------------------------------------------------------------------------------------
+def get_centerpoint_location_for_plotting(location):
+    print ('Location: ', location)
+    if (location == 'A'):
+        x_val = a_circle[0]
+        y_val = a_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+    elif (location == 'B'):
+        x_val = b_circle[0]
+        y_val = b_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+    elif (location == 'C'):
+        x_val = c_circle[0]
+        y_val = c_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+    elif (location == 'D'):
+        x_val = d_circle[0]
+        y_val = d_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+    elif (location == 'E'):
+        x_val = e_circle[0]
+        y_val = e_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+    elif (location == 'F'):
+        x_val = f_circle[0]
+        y_val = f_circle[1]
+        print('---> x_val: ', x_val, " y_val: ", y_val)
+
+    plot_detection_circle(x_val, y_val, circle_r)
+
+    # ------------------------------------------------->
+def plot_detection_circle(x, y, circle_radius):
+
+    print ("X: ", x, " Y: ", y)
+    #plt.figure()
+    #ax1 = fig1.add_subplot(111, aspect='equal')
 
 
-mpl_fig = plt.figure()
-ax = mpl_fig.add_subplot(111)
-x = np.linspace(-10, 10, num=50)
-y = np.sin(x)
+    circle = plt.Circle((x, y), radius=circle_radius, color='red', fill=False)
+    fig, ax = plt.subplots()
+    ax.add_artist(circle)
+    plt.show()
+    #ax = plt.gca()
+    #ax.cla()
+    #ax.add_artist(circle)
+    #ax.show()
 
-line, = ax.plot(x, y, lw=1)
+    # plt.plot(circle)
+    # plt.figure(1, figsize=(L,W))
+    # plt.figure(figsize=(4, 3))
+    # plt.figure(figsize=(40, 30), dpi=500)
+    #plt.xlabel('X Axis (In.)')
+    #plt.ylabel('Y Axis (In.)')
+    #plt.title('Object Detection')
+    #plt.grid(True)
+    # plt.savefig("sin_wave.png", transparent=True)
+    #plt.show()
+    return
 
-ax.set_title("Plot with Transparent Background")
+# <-------------------------------------------------
 
-plotly_fig =tls.mpl_to_plotly(mpl_fig)
+get_centerpoint_location_for_plotting(location)
 
-plotly_fig = tls.mpl_to_plotly( mpl_fig )
-plotly_fig["layout"].update({
-                             "plot_bgcolor":"rgba(0, 0, 0, 0)",
-                             "paper_bgcolor":"rgba(0,0,0,0)"
-                             })
-
-py.plot(plotly_fig)
-'''
-t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-plt.plot(t, s)
-
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('Graph Practice')
-plt.grid(True)
-plt.savefig("sin_wave.png", transparent=True)
-plt.show()
-'''
+#plot_detection_circle(4, 3, 10)
