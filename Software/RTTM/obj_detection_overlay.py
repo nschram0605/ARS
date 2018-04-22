740182bf000a18bd4438b35f57e75fad179b2ba7
Nathan <nschram05@gmail.com>
2018-04-21 21:20:01 -0600

    Adding obj_detection_overlay.py.

--- Software/RTTM/obj_detection_overlay.py
+++ Software/RTTM/obj_detection_overlay.py
@@ -19,3 +19,14 @@ import matplotlib.pyplot as plt
 #--------------------     ---
 #
 # Center points will be used for
+
+t = np.arrange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('Graph Practice')
+plt.grid(True)
+#plt.savefig("sin_wave.pgn")
+plt.show()
