ab379fa277c7bfdaafe5c49787d48c9e93bad234
Ryan Heifferon <heff@Macintoshs-MacBook-Pro.local>
2018-05-02 16:10:48 -0600

    added 'alpha' keyword for transparency

--- Software/RTTM/obj-det-ol.py
+++ Software/RTTM/obj-det-ol.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
 def create_circle():
-    circle=plt.Circle(0,0), radius=5)
+    circle=plt.Circle((0,0), alpha=0.5, radius=5) # alpha sets opaqueness
     return circle
+
+def create_circle1():
+    circle1=plt.Circle((0,0), color='r', radius=2)
+    return circle1
 
 def show_shape(patch):
     ax=plt.gca()
